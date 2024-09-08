e6c1bac2ec1e23339d200d21479635db5c8d465a
4ae4eK-2020 <melkov2019a@inbox.ru>
2024-09-08 12:46:53 +0500

    add change picture visibility button

--- app/src/main/java/com/example/lr_1_melkov_andrey/MainActivity.java
+++ app/src/main/java/com/example/lr_1_melkov_andrey/MainActivity.java
@@ -1,31 +1,39 @@
 package com.example.lr_1_melkov_andrey;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button changeVisibilityBtn;
+    private Button changeTextVisibilityBtn;
     private TextView textViewGroup, textViewName;
     private boolean isVisibleNow = true;
+    private ImageButton changePictureVisibilityBtn;
+    private ImageView imageCenterView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        changeVisibilityBtn = findViewById(R.id.changeVisibilityBtn);
+        changeTextVisibilityBtn = findViewById(R.id.changeVisibilityBtn);
         textViewGroup = findViewById(R.id.textViewGroup);
         textViewName = findViewById(R.id.textViewName);
 
-        changeVisibilityBtn.setOnClickListener(new View.OnClickListener() {
+        changePictureVisibilityBtn = findViewById(R.id.changePictureVisibilityBtn);
+        imageCenterView = findViewById(R.id.imageCenterView);
+
+        changeTextVisibilityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isVisibleNow == true) {
+                if(isVisibleNow) {
                     textViewGroup.setVisibility(View.INVISIBLE);
                     textViewName.setVisibility(View.INVISIBLE);
                 } else {
